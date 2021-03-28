cc7266af6ea8012e24e6f0099466cf0640af82b1
PRAMOD A JINGADE <avj2352@gmail.com>
2021-03-28 20:17:36 +0800

    created automation scripts

---
+++ automation/ss-wedding-scripts/api-clean.sh
@@ -0,0 +1,10 @@
+echo "Deleting source files...."
+rm -rf ../../ss-wedding/node_modules
+rm -rf ../../ss-wedding/routes
+rm -rf ../../ss-wedding/controllers
+rm -rf ../../ss-wedding/services
+rm -rf ../../ss-wedding/util
+rm  ../../ss-wedding/index.js
+rm  ../../ss-wedding/index.js.map
+rm ../../ss-wedding/yarn.lock
+rm ../../ss-wedding/package-lock.json
---
+++ automation/ss-wedding-scripts/ui-deploy.sh
@@ -0,0 +1,3 @@
+echo "UI - Copying from Frontend..."
+cp -a ../../../react-workspace/priti-vignesh-wedding-site/src/public/. ../../ss-wedding/public
+echo "Copied! "
