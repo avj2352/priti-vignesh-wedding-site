89cd32a4a6c9132777c830fc9ecfb4f28b1f4517
Pramod AJ <avj2352@gmail.com>
2021-03-30 17:23:36 +0800

    Changed chat messages model. Completed post to firebase

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -1,5 +1,8 @@
+
 (function($) {
     $(document).ready(function () {
+
+        const EVENT_NAME = 'Event'; // change for every page
 
         // API Calls ===================================================
         
@@ -11,7 +14,32 @@
             const request = await fetch(path);
             const data = await request.json();
             return data;
-        }        
+        }
+        
+        /**
+         * API call to submit Chat message to Firebase
+         * @param {name, message, event, isAttending} content          
+         * @returns Promise<any>
+         */
+        async function postChatMessage (data) {
+            const promise = new Promise((resolve, reject)=> {
+                fetch('/messages', {
+                    method: 'POST', // or 'PUT'
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    resolve(data);
+                })
+                .catch((error) => {
+                    reject('Error:', error);
+                });
+            });
+            return promise;
+        }
 
         // HELPER FUNCTIONS ============================================
         
@@ -107,6 +135,63 @@
             // link map
         }
 
+        // CHAT RELATED scripts ============================================
+
+        /**
+         * Function to validate the input fields
+         * @returns boolean
+         */
+        function validateForm () {
+            const nameFieldDom = document.getElementById('name');
+            const textFieldDom = document.getElementById('message');
+            const checkboxDom = document.getElementById('isAttending');
+            // console.log('Data within these fields is: ', nameFieldDom.value, textFieldDom.value);
+            if (
+                nameFieldDom.value.trim() == '' || 
+                textFieldDom.value.trim() == '' || 
+                textFieldDom.value.length <= 3 || 
+                nameFieldDom.value.length < 3
+                ) return true;
+            else return false;
+        }
+
+
+        /**
+         * Function to display success result
+         * @returns 
+         */
+        function displaySuccess () {
+            $('.validationText').hide();
+            $('#wishForm').hide();
+            return $("#successMsg").show();
+        }
+
+        /**
+         * Submit Chat - Post Message to Firebase
+         */
+        async function onChatFormSubmission() {
+            console.log('jQuery submit form!');
+            if (validateForm()) return $('.validationText').show();
+            else {
+                const nameFieldDom = document.getElementById('name');
+                const textFieldDom = document.getElementById('message');
+                const checkboxDom = document.getElementById('isAttending');
+                // console.log('Wish panel data present, submitting: ', nameFieldDom.value, textFieldDom.value, checkboxDom.checked);
+                // Posting XHR request
+                const payload = {
+                    name: nameFieldDom.value,
+                    message: textFieldDom.value,
+                    event: EVENT_NAME,
+                    isAttending: checkboxDom.checked
+                };
+                try {
+                    const result = await postChatMessage(payload);
+                    if (result) return displaySuccess();
+                } catch (err) {
+                    throw new Error ('Error submitting the form: ', err);
+                }                
+            }
+        }
 
         // INIT scripts ====================================================
         
@@ -123,6 +208,9 @@
             populateEventAddressContent(fb, 'wedding');
             populateEventAddressContent(fb, 'lunch');
             populateEventAddressContent(fb, 'reception');
+
+            // Call chat submission
+            $('#wishForm').submit(onChatFormSubmission);
         }
 
         // call main method
